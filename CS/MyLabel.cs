7cdf7194db18d0cba17ac7d4d1195ee302d2cc34
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2022-06-15 12:52:00 -0700

    auto sync for version 2022.1

--- CS/MyLabel.cs
+++ CS/MyLabel.cs
@@ -5,23 +5,24 @@ using DevExpress.XtraReports.UI;
 using DevExpress.XtraPrinting;
 using DevExpress.XtraPrinting.BrickExporters;
 using System.Drawing;
+using DevExpress.Drawing;
 
 namespace Q305659
 {
     public class MyLabel : XRLabel {
         protected override DevExpress.XtraPrinting.VisualBrick CreateBrick(DevExpress.XtraPrinting.VisualBrick[] childrenBricks)
         {
-            return new MaLabelBrick();
+            return new MyLabelBrick();
         }
     }
     [BrickExporter(typeof(MyLabelBrickExporter))]
-    public class MaLabelBrick : LabelBrick
+    public class MyLabelBrick : LabelBrick
     { 
     }
 
     public class MyLabelBrickExporter : VisualBrickExporter {
         protected override void DrawClientContent(IGraphics gr, RectangleF clientRect) {
-            gr.DrawString("MyLabel string", ((VisualBrick)this.Brick).Style.Font, Brushes.Red, clientRect.Location);
+            gr.DrawString("MyLabel string", VisualBrick.Style.Font, DXBrushes.Red, clientRect, VisualBrick.Style.StringFormat.DXValue);
         }
     }
 }
--- VB/MyLabel.vb
+++ VB/MyLabel.vb
@@ -2,6 +2,7 @@ Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections.Generic
 Imports System.Text
+Imports DevExpress.Drawing
 Imports DevExpress.XtraReports.UI
 Imports DevExpress.XtraPrinting
 Imports DevExpress.XtraPrinting.BrickExporters
@@ -11,18 +12,18 @@ Namespace Q305659
 	Public Class MyLabel
 		Inherits XRLabel
 		Protected Overrides Function CreateBrick(ByVal childrenBricks() As DevExpress.XtraPrinting.VisualBrick) As DevExpress.XtraPrinting.VisualBrick
-			Return New MaLabelBrick()
+			Return New MyLabelBrick()
 		End Function
 	End Class
 	<BrickExporter(GetType(MyLabelBrickExporter))> _
-	Public Class MaLabelBrick
+	Public Class MyLabelBrick
 		Inherits LabelBrick
 	End Class
 
 	Public Class MyLabelBrickExporter
 		Inherits VisualBrickExporter
 		Protected Overrides Sub DrawClientContent(ByVal gr As IGraphics, ByVal clientRect As RectangleF)
-			gr.DrawString("MyLabel string", (CType(Me.Brick, VisualBrick)).Style.Font, Brushes.Red, clientRect.Location)
+			gr.DrawString("MyLabel string", VisualBrick.Style.Font, DXBrushes.Red, clientRect, VisualBrick.Style.StringFormat.DXValue)
 		End Sub
 	End Class
 End Namespace
